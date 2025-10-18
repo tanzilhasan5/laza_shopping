8c023c113ce7e6a48c34808bab0eeed96a7915bf
tanzil <tanzilhasan151@gmail.com>
2025-10-19 00:36:29 +0600

    final

--- lib/ui/widgets/CustomAuthWidgets/custom_textfield.dart
+++ lib/ui/widgets/CustomAuthWidgets/custom_textfield.dart
@@ -2,40 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../../utils/appColor.dart';
 
-/*
-class CustomTextField extends StatelessWidget {
-  CustomTextField({
-    super.key,
-    this.controller,
-    this.hintText,
-    this.labelText,
-    this.obsecureText=false,
-  });
-  final TextEditingController? controller;
-  final String? labelText;
-  final String? hintText;
-  bool obsecureText;
-
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      obscureText: obsecureText,
-      decoration:InputDecoration(
-        hintText: hintText,hintStyle: TextStyle(color:Colors.grey,fontSize: 13),
-        labelText: labelText,labelStyle: TextStyle(color:Colors.grey,fontSize: 13),
-        enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey)
-        ),
-        focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey)
-        ),
-      ),
-    );
-  }
-}*/
-
 class CustomTextField extends StatelessWidget {
   String? Function(String?)? validator;
 
@@ -60,13 +26,6 @@ class CustomTextField extends StatelessWidget {
     return TextFormField(
       controller: controller,
       validator: validator ,
-         /* (value){
-        if (value == null || value.isEmpty){
-          return  returnText;
-        }else{
-          return null;
-        }
-      },*/
       decoration:InputDecoration(
           hintText: hintText,hintStyle: TextStyle(color: AppColor.textColor,fontSize: 11.95),
           labelText: labelText,labelStyle: TextStyle(fontSize: 15,color: Colors.black),
@@ -100,32 +59,4 @@ class CustomTextField extends StatelessWidget {
     );
   }
 }
-/*
-enum Validator {
-  email,
-  changePassword,
-}
-
-String? validateInput(String? value,) {
-  if (value == null || value.isEmpty) {
-    return 'Field cannot be empty';
-  }
-
-  switch (type) {
-    case Validator.login:
-      if (!value.contains('@')) return 'Enter a valid email address';
-      break;
-
-    case Validator.signup:
-      if (value.length < 6) return 'Password must be at least 6 characters';
-      break;
-
-    case Validator.changePassword:
-      if (value.length < 8) return 'New password must be at least 8 characters';
-      break;
-  }
-
-  return null;
-}
-*/
 
